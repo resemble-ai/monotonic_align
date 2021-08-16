8db00095bee9a22d6c560389a0bf49b3f737a43f
Jeremy Hsu <jeremycchsu@gamil.com>
2021-08-16 04:31:45 -0400

    make it pip-installable

---
+++ monotonic_align/__init__.py
@@ -0,0 +1,5 @@
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution('monotonic_align').version
+
+from monotonic_align.mas import maximum_path
--- monotonic_align/mas.py
+++ monotonic_align/mas.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from .monotonic_align.core import maximum_path_c
+from monotonic_align.core import maximum_path_c
 
 
 def maximum_path(value, mask):  
@@ -14,7 +14,6 @@ def maximum_path(value, mask):
   value = value.data.cpu().numpy().astype(np.float32)
   path = np.zeros_like(value).astype(np.int32)
   mask = mask.data.cpu().numpy()
-
   t_x_max = mask.sum(1)[:, 0].astype(np.int32)
   t_y_max = mask.sum(2)[:, 0].astype(np.int32)
   maximum_path_c(path, value, t_x_max, t_y_max)
