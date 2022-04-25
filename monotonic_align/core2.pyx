021acaf00da42eefc82f29db3b7a9f0041337bbd
JeremyCCHsu <jeremycchsu@gmail.com>
2022-04-25 05:40:05 -0500

    replace while with for

--- monotonic_align/core2.pyx
+++ monotonic_align/core2.pyx
@@ -2,7 +2,6 @@ import numpy as np
 cimport numpy as np
 cimport cython
 from cython.parallel import prange
-# from libc.stdio cimport printf
 
 
 @cython.boundscheck(False)
@@ -10,8 +9,6 @@ from cython.parallel import prange
 cdef void maximum_path_each2(int[:,::1] path, float[:,::1] value, int n_symbols, int n_frames, float max_neg_val) nogil:
   cdef int x
   cdef int y
-
-  cdef int yy
 
   cdef float v_prev_symbol
   cdef float v_prev_state
@@ -26,8 +23,6 @@ cdef void maximum_path_each2(int[:,::1] path, float[:,::1] value, int n_symbols,
   cdef float skip_eps_transition = 0.0
   cdef float max_ = 0.0
   cdef float max_val = 0.0
-
-  cdef int last_frame_index = n_frames - 1
 
   # dynamic programming
   for frame_index in range(n_frames):
@@ -67,57 +62,31 @@ cdef void maximum_path_each2(int[:,::1] path, float[:,::1] value, int n_symbols,
       value[symbol_index, frame_index] = max(max_, v_prev_symbol) + value[symbol_index, frame_index]
 
 
-  # # backtracking (indicator matrix)
-  # for yy in range(last_frame_index, -1, 1):
-  #   path[index, yy] = 1
-  #   if index > 2:
-
-
-
-  # BUG: THE TRANSITION IS WEIRD! ONLY THE symbol_index % 2 == 0 should have esp-trans
-
-  yy = last_frame_index
-  while index > 2:
-    path[index, yy] = 1
+  # backtracking (indicator matrix)
+  for frame_index in range(n_frames - 1, -1, -1):
+    path[index, frame_index] = 1
 
     # must transition due to monotonic constraint
-    if index == yy:
+    if index == frame_index:
       index = index - 1
 
-    # transition priority: phone > epi > stay
-    stay = value[index, yy - 1]
-    unit_step = value[index - 1, yy - 1]
-    skip_eps_transition = value[index - 2, yy - 1]
+    if index != 0:
+      if frame_index > 0:
+        stay = value[index, frame_index - 1]
+        unit_step = value[index - 1, frame_index - 1]
 
-    max_ = max(stay, unit_step)
-    if index % 2 == 0:
-      max_val = max(max_, skip_eps_transition)
-      if skip_eps_transition == max_val:
-        index = index - 2
-      else:
-        if unit_step == max_val:
-          index = index - 1
-    else:
-      if unit_step == max_:
-        index = index - 1
-      # else:
-      #   # if stay == max_val:
-      #   #   index = index - 1
-      #   # else:
-      #   #   index = index - 0
-      #   index = index - 0
-      #   # index -= 2
-
-
-    yy = yy -1
-  # else:
-  #   break
-
-  # FIXME
-  for y in range(yy, -1, -1):
-    path[index, y] = 1
-    if index != 0 and (index == y or value[index, y-1] < value[index-1, y-1]):
-      index = index - 1
+        max_ = max(stay, unit_step)
+        if (index >= 2) and (index % 2 == 0):
+          skip_eps_transition = value[index - 2, frame_index - 1]
+          max_val = max(max_, skip_eps_transition)
+          if skip_eps_transition == max_val:
+            index = index - 2
+          else:
+            if unit_step == max_val:
+              index = index - 1
+        else:
+          if unit_step == max_:
+            index = index - 1
 
 
 @cython.boundscheck(False)
