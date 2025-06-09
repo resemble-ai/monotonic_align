42f2f78092ba8cb8e1133ef6f3b5c7764f6f31d1
JeremyCCHsu <jeremycchsu@gmail.com>
2025-06-09 09:06:14 +0000

    add __version__

--- monotonic_align/__init__.py
+++ monotonic_align/__init__.py
@@ -1,1 +1,8 @@
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # For Python <3.8
+
+__version__ = version("monotonic_align")
+
 from monotonic_align.mas import *
